a8dfcce3d3301411c9c34a7ca23d9a9de40b5317
srphillips <scott.phillips@aggiemail.usu.edu>
2015-11-19 12:07:36 -0700

    Revert "Test"
    This reverts commit a4cc2a2e72d27f22534f94db448b061acb9ea2dc, reversing
    changes made to 86c68c0a16633798e74bd7ddcb4112710d233404.

--- Blackjack/Properties/Resources.Designer.cs
+++ Blackjack/Properties/Resources.Designer.cs
@@ -87,5 +87,15 @@ namespace Blackjack.Properties {
                 return ResourceManager.GetStream("cardsShuffling", resourceCulture);
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Mainbjack {
+            get {
+                object obj = ResourceManager.GetObject("Mainbjack", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
